7535843ebc9f311ec76933c6a0a681b64eadd0eb
Jeffrey Pak <jeffrey.pak@emc.com>
2017-07-25 14:17:42 -0700

    update script to install protoc - protocol compiler
    [#149091715](https://www.pivotaltracker.com/story/show/149091715)

--- scripts/generate-csi-proto.sh
+++ scripts/generate-csi-proto.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+wget https://github.com/google/protobuf/releases/download/v3.3.0/protoc-3.3.0-linux-x86_64.zip
+unzip protoc-3.3.0-linux-x86_64.zip
+mv bin/protoc /usr/bin
+
 pushd csi-spec
   go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
-  export PATH=$PATH:$HOME/src/go/bin
   make csi.proto
   make csi.pb.go
 popd
