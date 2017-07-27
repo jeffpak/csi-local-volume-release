948bddf121e9f96f9e89c64fccd41dfe2b4fcd07
Kai Xiang <kxiang@pivotal.io>
2017-07-27 16:33:52 -0700

    go get protoc-gen-go as it's not submoduled in
    [#149700869](https://www.pivotaltracker.com/story/show/149700869)
    
    Signed-off-by: Julian Hjortshoj <julian.hjortshoj@emc.com>

--- scripts/generate-csi-proto.sh
+++ scripts/generate-csi-proto.sh
@@ -2,7 +2,8 @@
 set -ex
 
 pushd $(dirname $0)/../src/github.com/container-storage-interface/spec
-  go install github.com/golang/protobuf/{proto,protoc-gen-go}
+  go install github.com/golang/protobuf/proto
+  go get -u github.com/golang/protobuf/protoc-gen-go
   make csi.proto
   make csi.pb.go
 popd
