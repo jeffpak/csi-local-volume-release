520d2741c9d43a07b001ba1da207c64a77d69887
Kai Xiang <kxiang@pivotal.io>
2017-07-28 13:55:54 -0700

    go install proto instead of go get as it's already submoduled
    [149700869](https://www.pivotaltracker.com/story/show/149700869)

--- scripts/generate-csi-proto.sh
+++ scripts/generate-csi-proto.sh
@@ -2,6 +2,8 @@
 set -ex
 
 pushd $(dirname $0)/../src/github.com/container-storage-interface/spec
+  go get -u github.com/golang/protobuf/protoc-gen-go
+  go install github.com/golang/protobuf/proto
   make csi.proto
   make csi.pb.go
 popd
