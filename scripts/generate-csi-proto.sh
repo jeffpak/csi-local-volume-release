bad483dbb92663ef54e08f11fc0358737f936816
Kai Xiang <kxiang@pivotal.io>
2017-07-28 13:52:25 -0700

    remove go get in favor of git submodules
    [149700869](https://www.pivotaltracker.com/story/show/149700869)

--- scripts/generate-csi-proto.sh
+++ scripts/generate-csi-proto.sh
@@ -2,7 +2,6 @@
 set -ex
 
 pushd $(dirname $0)/../src/github.com/container-storage-interface/spec
-  go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
   make csi.proto
   make csi.pb.go
 popd
