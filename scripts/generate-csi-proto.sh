443465dc11e29e5e9cdb83e32967ba0ff6722bd7
Kai Xiang <kxiang@pivotal.io>
2017-07-28 14:00:12 -0700

    go install proto and protoc-gen-go to fix the ci test
    [149700869](https://www.pivotaltracker.com/story/show/149700869)

--- scripts/generate-csi-proto.sh
+++ scripts/generate-csi-proto.sh
@@ -2,8 +2,7 @@
 set -ex
 
 pushd $(dirname $0)/../src/github.com/container-storage-interface/spec
-  go get -u github.com/golang/protobuf/protoc-gen-go
-  go install github.com/golang/protobuf/proto
+  go install github.com/golang/protobuf/{proto,protoc-gen-go}
   make csi.proto
   make csi.pb.go
 popd
