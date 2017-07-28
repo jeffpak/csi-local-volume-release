4ae419de595b2691657d63881571368d56cc23fa
Kai Xiang <kxiang@pivotal.io>
2017-07-28 11:21:53 -0700

    use trap in the run_csi_cert script so it cleanup after failures
    [149700869](https://www.pivotaltracker.com/story/show/149700869)

--- scripts/run_csi_cert.sh
+++ scripts/run_csi_cert.sh
@@ -2,7 +2,10 @@
 
 set -ex
 
-cd `dirname $0`
+script_path=`realpath $0`
+script_dir=`dirname $script_path`
+
+cd $script_dir
 
 ./generate-csi-proto.sh
 
@@ -16,12 +19,17 @@ go get -t github.com/paulcwarren/csi-cert
 # local-controller-plugin 
 #=======================================================================================================================
 
+function cleanup {
+  cd $script_dir
+  /bin/bash ./stop_controller_plugin_tcp.sh
+  rm -rf $HOME/csi_plugins
+}
+
 # TCP TESTS
 export FIXTURE_FILENAME=$PWD/scripts/fixtures/local_plugin_cert.json
+trap cleanup EXIT
 /bin/bash scripts/start_controller_plugin_tcp.sh
 pushd src/github.com/paulcwarren/csi-cert
     ginkgo -r -p
 popd
-/bin/bash scripts/stop_controller_plugin_tcp.sh
 
-rm -rf $HOME/csi_plugins
