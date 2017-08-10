6a2a4aa16c8156f7fbdde765813da5fa45829490
Kai Xiang <kxiang@pivotal.io>
2017-08-10 14:46:58 -0700

    Remove csi proto buff code temporary
    [150030002](https://www.pivotaltracker.com/story/show/150030002)
    
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- scripts/generate-csi-proto.sh
+++ scripts/generate-csi-proto.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -ex
 
-pushd $(dirname $0)/../src/github.com/container-storage-interface/spec
-  go install github.com/golang/protobuf/{proto,protoc-gen-go}
-  make csi.proto
-  make csi.pb.go
-popd
-
+echo "TODO: REMOVE THIS SCRIPT"
